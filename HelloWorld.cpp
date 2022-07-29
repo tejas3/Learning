51719270b7257f68345a99330a21d65dd709848a
rajesh3071 <rajeshpatil686868@gmail.com>
2022-07-29 13:04:31 +0530

    multiplication

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -2,10 +2,15 @@
 #include<string>
 using namespace std;
 
+int multiplication(int a,int b){
+   return a*b;
+}
+
 
 int main()
 {
 	string name="CPP";
 	cout << "Hello World"<<endl;
+     cout<<multiplication(10,10)<<endl;
 	return 0;
 }
