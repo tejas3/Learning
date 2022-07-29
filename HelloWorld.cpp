bc922cc788b48403df104087683e33482e96df5b
uddandu <uddandu887@gmail.com>
2022-07-29 12:42:46 +0530

    adding sum function

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
 #include<string>
 using namespace std;
-
+int sum(int a, int b){
+    return a+b;
+}
 
 int main()
 {
 	string name="CPP";
-	cout << "Hello World"<<endl;
+        cout << sum(10,20)<<endl;
+
 	return 0;
 }
