8398d6fdd652aeafbc6373c5917f294b60817132
keshwarya <keshwaryakunjekar98@gmail.com>
2022-07-29 12:43:48 -0700

    Adding Sum Function

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -2,10 +2,13 @@
 #include<string>
 using namespace std;
 
+int sum(int a,int b){
+return a+b;
+}
 
 int main()
 {
 	string name="CPP";
-	cout << "Hello World"<<endl;
+	cout << sum(10,20)<<endl;
 	return 0;
 }
